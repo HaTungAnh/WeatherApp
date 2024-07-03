601071fb5bacece99288f1ae34bba2d022229979
HaTungAnh <21021453@vnu.edu.vn>
2024-07-03 20:59:25 +0700

    feat(state & viewmodel)

--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
@@ -1,7 +1,6 @@
 package com.plcoding.weatherapp.presentation
 
 import com.plcoding.weatherapp.domain.weather.WeatherInfo
-import java.lang.Error
 
 data class WeatherState(
     val weatherInfo: WeatherInfo? = null,
